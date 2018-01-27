cc760685c954d300a16d3e7923a3feb54200841d
Claine <7941324@qq.com>
2018-01-27 14:28:32 +0800

    Excel配置表解析
    1.修改为直接转换为lua

--- Assets/Editor/Config/ExcelParse.cs
+++ Assets/Editor/Config/ExcelParse.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using NPOI.SS.UserModel;
 using NPOI.HSSF.UserModel;
@@ -11,49 +12,72 @@ public class ExcelParse
     [MenuItem("MyTool/ConfigParse")]
     private static void ConfigParse()
     {
-        IWorkbook workbook = null;  
-        List<string> tmpDataList = new List<string>();
-        string[] excelFiles = Directory.GetFiles(ConfigConst.ExcelResPath,"*.xlsx");
-        ConfigAsset configAsset = ScriptableObject.CreateInstance<ConfigAsset>();
-        
+        if (Directory.Exists(ConfigConst.ConfigResPath))
+            Directory.Delete(ConfigConst.ConfigResPath, true);
+        Directory.CreateDirectory(ConfigConst.ConfigResPath);
+
+        string[] excelFiles = Directory.GetFiles(ConfigConst.ExcelResPath, "*.xlsx");
         for (int i = 0; i < excelFiles.Length; i++)
         {
-            string fileName = excelFiles[i];
-            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-            if (fileName.IndexOf(".xlsx") > 0) // 2007版本  
-            {
-                workbook = new XSSFWorkbook(fileStream);  //xlsx数据读入workbook  
-            }
-            else if (fileName.IndexOf(".xls") > 0) // 2003版本  
-            {
-                workbook = new HSSFWorkbook(fileStream);  //xls数据读入workbook  
-            }
+            FileStream fileStream = new FileStream(excelFiles[i], FileMode.Open, FileAccess.Read);
+            IWorkbook workbook = new XSSFWorkbook(fileStream);
+            fileStream.Close();
 
             ISheet sheet = workbook.GetSheetAt(0);  //获取第一个工作表  
-            int rowNum = sheet.LastRowNum + 1;
-            IRow iRow;
-            for (int row = 0; row < rowNum; row++)  //对工作表每一行  
+            IRow iRowType = sheet.GetRow(0);
+            IRow iRowName = sheet.GetRow(2);
+            int colNum = iRowType.LastCellNum;
+            string[] dataType = new string[colNum];
+            string[] dataName = new string[colNum];
+            for (int col = 0; col < colNum; col++)
             {
-                iRow = sheet.GetRow(row);   //row读入第i行数据  
+                dataType[col] = iRowType.GetCell(col).ToString();
+                dataName[col] = iRowName.GetCell(col).ToString();
+            }
+            string tableData = "{0} = {{\n{1}}}";
+            string rowKey = "  {0}_{1} = {{\n{2}  }},\n";
+
+            string rowDataStr = string.Empty;
+            for (int row = 3; row < sheet.LastRowNum + 1; row++)  //对工作表每一行  
+            {
+                IRow iRow = sheet.GetRow(row);   //row读入第i行数据  
                 if (iRow != null)
                 {
-                    for (int col = 0; col < iRow.LastCellNum; col++)  //对工作表每一列  
+                    string rowData = string.Empty;
+                    string dataFormat = string.Empty;
+                    for (int col = 0; col < colNum; col++)  //对工作表每一列  
                     {
-                        tmpDataList.Add(iRow.GetCell(col).ToString());
+                        switch (dataType[col])
+                        {
+                            case "int":
+                                {
+                                    dataFormat = "    {0} = {1},\n";
+                                    break;
+                                }
+                            case "float":
+                                {
+                                    dataFormat = "    {0} = {1:0.0000},\n";
+                                    break;
+                                }
+                            case "string":
+                                {
+                                    dataFormat = "    {0} = '{1}',\n";
+                                    break;
+                                }
+                            case "table":
+                                {
+                                    dataFormat = "    {0} = {{{1}}},\n";
+                                    break;
+                                }
+                        }
+                        rowData += String.Format(dataFormat, dataName[col], iRow.GetCell(col));
                     }
+                    rowDataStr += string.Format(rowKey, dataName[0], iRow.GetCell(0), rowData);
                 }
             }
-
-            configAsset.Name = sheet.SheetName;
-            configAsset.MaxRow = rowNum;
-            configAsset.MaxCol = tmpDataList.Count / rowNum;
-            configAsset.Datas = tmpDataList.ToArray();
-
-            if (File.Exists(ConfigConst.ExcelResPath))
-                File.Delete(ConfigConst.ExcelResPath);
-
-            AssetDatabase.CreateAsset(configAsset, ConfigConst.ConfigResPath + configAsset.Name + ".asset");
-            fileStream.Close();
+            string luaPath = ConfigConst.ConfigResPath + sheet.SheetName + ".lua";
+            string luaContent = string.Format(tableData, sheet.SheetName, rowDataStr).Trim();
+            File.WriteAllText(luaPath, luaContent);
             workbook.Close();
         }
         AssetDatabase.Refresh();
