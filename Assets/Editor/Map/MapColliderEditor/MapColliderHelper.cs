65f414a71dded15d90198d39c8e65a616dbb16b2
caibinbin <390750806@qq.com>
2018-02-04 15:36:59 +0800

    修复碰撞盒在游戏中会便宜问题 添加角色创建位置接口

--- Assets/Editor/Map/MapColliderEditor/MapColliderHelper.cs
+++ Assets/Editor/Map/MapColliderEditor/MapColliderHelper.cs
@@ -17,16 +17,24 @@ public class MapColliderHelper
         if (_mapBlockData != null)
         {
             string mapData = string.Empty;
+            string mapRoleCreatePoint = string.Empty;
             for (int i = 0; i < _mapBlockData.Count; i++)
             {
                 if (_mapBlockData[i].type == eMapBlockType.None)
                     continue;
 
                 mapData += _mapBlockData[i] + "\n";
+                if (_mapBlockData[i].type == eMapBlockType.playerPoint)
+                    mapRoleCreatePoint += _mapBlockData[i] + "\n";
             }
             if (File.Exists(MapDefine.MapDataSavePath))
                 File.Delete(MapDefine.MapDataSavePath);
             File.WriteAllText(MapDefine.MapDataSavePath, mapData.Trim());
+
+            if (File.Exists(MapDefine.MapRoleCreatePointSavePath))
+                File.Delete(MapDefine.MapRoleCreatePointSavePath);
+            File.WriteAllText(MapDefine.MapRoleCreatePointSavePath, mapRoleCreatePoint.Trim());
+
             AssetDatabase.Refresh();
         }
         else
