7086adef728ed86488eef953dd8da3f521228d70
ClaineLe <7941324@qq.com>
2018-02-06 19:23:15 +0800

    登录界面

---
+++ Assets/Scripts/Panel/UILoginPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILoginPanel : MonoBehaviour
+{
+	private Button _btnLogin;
+
+	private InputField _inputFieldAccount;
+
+	private Widget _widget;
+
+	private void Start()
+	{
+		GameManager.GetInstance ().Init ();
+
+		LoginModule.GetInstance ().LoginPanel = this;
+
+		Init ();
+	}
+
+	public void Init()
+	{
+		_widget = Widget.Create(gameObject);
+		_widget.BandingBtn_OnClick (OnBtnClick_Login,"ViewPort/Btn_Login");
+		_inputFieldAccount = _widget.GetComponent<InputField> ("ViewPort/Account/InputField");
+	}
+
+	private void OnBtnClick_Login()
+	{
+		LoginModule.GetInstance ().NetWork_Request_Login ("ssss","");
+	}
+
+}
+   
