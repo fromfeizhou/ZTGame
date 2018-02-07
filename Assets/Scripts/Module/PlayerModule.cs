a3f25c493ae8eef8fb955cd85a498a7346c8ae1b
ClaineLe <7941324@qq.com>
2018-02-07 15:28:02 +0800

    玩家信息模块

---
+++ Assets/Scripts/Module/PlayerModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModule : Singleton<PlayerModule>
+{
+	private gprotocol.p_role _roleInfo;
+
+	public uint RoleID
+	{
+		get{ 
+			return _roleInfo.id;
+		}
+	}
+	public void SetRoleInfo(gprotocol.p_role roleInf){
+		_roleInfo = roleInf;
+	}
+}
