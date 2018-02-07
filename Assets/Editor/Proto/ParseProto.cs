df92d5e4329f79957c909cf5f74c505ef8ba46eb
ClaineLe <7941324@qq.com>
2018-02-07 19:22:42 +0800

    proto协议解析类定义迁移

--- Assets/Editor/Proto/ParseProto.cs
+++ Assets/Editor/Proto/ParseProto.cs
@@ -8,56 +8,6 @@ using UnityEditor;
 
 public class ParseProto{
 
-	[System.Serializable]
-	class cCommand
-	{
-		public int moduleId;
-		public string moduleEn = string.Empty;
-
-		public string commandEn = string.Empty;
-		public string commandCn = string.Empty;
-
-		public int commandId;
-
-		public static cCommand Create(string str){
-			cCommand command = new cCommand ();
-			string tmpStr = str;
-			tmpStr = tmpStr.Substring (tmpStr.IndexOf ("#begin#") - 1);
-			tmpStr = tmpStr.Replace ("#begin#",string.Empty);
-			tmpStr = tmpStr.Replace ("#end#",string.Empty);
-			List<string> param = new List<string> (tmpStr.Trim ().Split ('#'));
-			param.RemoveAll(a=>string.IsNullOrEmpty(a));
-			command.moduleEn = param [0];
-			command.commandEn = param [1];
-			command.moduleId = int.Parse(param [2]);
-			command.commandId = int.Parse(param [3]);
-			command.commandCn = param [4];
-			return command;
-		}
-
-	}
-
-	[System.Serializable]
-	class cModule
-	{
-		public string moduleEn = string.Empty;
-		public string moduleCn = string.Empty;
-		public int moduleId;
-		public List<cCommand> commandList = new List<cCommand>();
-		public static cModule Create(string str){
-			cModule module = new cModule ();
-			string tmpStr = str;
-			tmpStr = tmpStr.Substring (tmpStr.IndexOf ("#model_begin#") - 1);
-			tmpStr = tmpStr.Replace ("#model_begin#",string.Empty);
-			tmpStr = tmpStr.Replace ("#model_end#",string.Empty);
-			List<string> param = new List<string> (tmpStr.Trim ().Split ('#'));
-			param.RemoveAll(a=>string.IsNullOrEmpty(a));
-			module.moduleEn = param [0];
-			module.moduleId = int.Parse(param [1]);
-			module.moduleCn = param [2];
-			return module;
-		}
-	}
 
 
 	private const string protoPath = "ProtoGen/protobuf.proto";
@@ -156,6 +106,41 @@ public class ParseProto{
 				Debug.LogError ("Found Module. moduleId:" + command.moduleId +  "moduleEn:" + command.moduleEn);
 		}
 		SaveCSharpProtocolFiles (moduleList);
+		AssetDatabase.Refresh ();
+	}
+
+	[MenuItem("Tools/ParseProtoLocalTest")]
+	private static void ParseToLocalTest()
+	{
+		List<cModule> moduleList = new List<cModule> ();
+
+		List<string> allLines = new List<string> (File.ReadAllLines (protoPath));
+		List<string> message = allLines.FindAll (a => a.Contains ("_s2c"));
+		for (int i = 0; i < message.Count; i++) {
+			message [i] = message [i].Replace ("message", string.Empty).Replace ("{", string.Empty).Replace ("}", string.Empty).Trim ();
+		}
+
+
+		List<string> contentsModel = allLines.FindAll (a => (a.Contains ("#model_begin#") && a.Contains ("#model_end#")));
+		for (int i = 0; i < contentsModel.Count; i++) {
+			moduleList.Add (cModule.Create (contentsModel[i]));
+		}
+
+		List<string> contentsCommand = allLines.FindAll (a => (a.Contains ("#begin#") && a.Contains ("#end#")));
+		for (int i = 0; i < contentsCommand.Count; i++) {
+			cCommand command = cCommand.Create (contentsCommand[i]);
+
+			int idx = moduleList.FindIndex(a=>a.moduleId == command.moduleId && a.moduleEn == command.moduleEn);
+			if (idx >= 0)
+				moduleList [idx].commandList.Add (command);
+			else
+				Debug.LogError ("Found Module. moduleId:" + command.moduleId +  "moduleEn:" + command.moduleEn);
+		}
+
+		for (int i = 0; i < moduleList.Count; i++) {
+			File.WriteAllText("Assets/Resources/TestProtocolInfo/" + moduleList[i].moduleEn + ".txt",JsonUtility.ToJson(moduleList[i]));
+		}
+
 		AssetDatabase.Refresh ();
 	}
 }
