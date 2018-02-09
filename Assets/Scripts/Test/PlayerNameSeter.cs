85bd5dc577efc1088adc8613defda5d463ff0e09
ClaineLe <7941324@qq.com>
2018-02-09 11:51:57 +0800

    战斗UI临时操作脚本

---
+++ Assets/Scripts/Test/PlayerNameSeter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameSeter : MonoBehaviour {
+	public Text txtPlayerName;
+
+	void Update()
+	{
+		if (string.IsNullOrEmpty (txtPlayerName.text)) {
+			txtPlayerName.text = PlayerModule.GetInstance ().RoleName;
+		}
+	}
+}
---
+++ Assets/Scripts/Test/SkillItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillItem : MonoBehaviour {
+	public Image SkillImage;
+	public int SkillId;
+
+	public GameObject light;
+	void Start()
+	{
+		if (SkillId == 1001)
+			SetSkill ();
+		
+		transform.Find("BtnSkill").GetComponent<Button> ().onClick.AddListener (()=>{
+			SetSkill();
+		});
+	}
+
+	private void SetSkill()
+	{
+		SkillImage.sprite = transform.Find("BtnSkill").GetComponent<Image>().sprite;
+		FightModule.GetInstance().SelectSkill(SkillId);
+	}
+
+	void Update()
+	{
+		light.SetActive (FightModule.GetInstance().CurSkillId == SkillId);
+	}
+}
