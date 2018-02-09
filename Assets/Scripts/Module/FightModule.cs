51347b1b8edc8b691a95abfe7bcc702b8994a7c8
ClaineLe <7941324@qq.com>
2018-02-09 11:52:15 +0800

    战斗模块

---
+++ Assets/Scripts/Module/FightModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightModule : Singleton<FightModule> {
+
+	private int _curSkillId;
+	public int CurSkillId{
+		get{ 
+			return _curSkillId;
+		}
+	}
+
+	public void SelectSkill(int skillId)
+	{
+		_curSkillId = skillId;
+	}
+}
