d2a69a40411800a4be531fdd8ef8a1953fa8524d
ClaineLe <7941324@qq.com>
2018-02-28 18:51:25 +0800

    增加，地图网络门户

---
+++ Assets/Scripts/Core/NetWorkManager/ModuleNetFacade/MapNetFacade.cs
@@ -0,0 +1,23 @@
+﻿using gprotocol;
+
+
+namespace com.game.client
+{
+	namespace network.facade
+	{
+		[NetFacadeAttribute(Module.map)]
+		public class MapNetFacade
+		{
+			[NetCommandAttribute(Command.map_switch)]
+			private void OnReceive_Map_Switch(int code, map_switch_s2c vo)
+			{
+				UnityEngine.Debug.Log ("请求场景切换(暂时没用)");
+			}
+
+			[NetCommandAttribute(Command.map_role_leave)]
+			private void OnReceive_Map_Role_Leave(int code, map_role_leave_s2c vo ){
+				UnityEngine.Debug.Log ("玩家离开视野");
+			}
+		}
+	}
+}
