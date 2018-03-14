6d4160f9fd03c91c6fc71d5ab7904c5f6442fc1a
fromfeizhou <763457385@qq.com>
2018-03-14 20:08:19 +0800

    【战斗】 镜头控制

--- Assets/Scripts/Common/GameTool.cs
+++ Assets/Scripts/Common/GameTool.cs
@@ -29,4 +29,28 @@ public class GameTool
         }
         Debug.Log(str);
     }
+
+    public static void LogError(params object[] args)
+    {
+        string str = "";
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (i == 0)
+            {
+                if (null != args[i])
+                    str = args[i].ToString();
+                else
+                    str = "null";
+            }
+            else
+            {
+                if (null != args[i])
+                    str = str + "  " + args[i].ToString();
+                else
+                    str = str + "  " + "null";
+            }
+        }
+        Debug.LogError(str);
+    }
 }
