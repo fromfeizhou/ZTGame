87b998df9e0b951c032e792a3819786d7ab16323
caibinbin <390750806@qq.com>
2018-03-15 10:20:14 +0800

    添加模型控制器BaseModelSprite

---
+++ Assets/Scripts/Character/BaseModelSprite.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EquipType
+{
+    Cloth,
+    Equip
+}
+
+
+public class EquipData
+{
+    public EquipType type;
+    public string path;
+
+}
+
+
+public class BaseModelSprite
+{
+    protected GameObject model;
+    protected Animator animator;
+    protected bool isTrans;
+
+    //创建
+    public virtual void CreateModel(string path) { }
+    //销毁
+    public  virtual  void RemoveAnimatorView() { }
+    //播放
+    public virtual void Play(string animationName) { }
+    //位置更新
+    public virtual void UpdatePos(Vector3 pos) { }
+    //旋转
+    public virtual void UpdateRotete(Vector3 dir) { }
+    //透明度
+    public  virtual  void ChangeTranslucence(bool isTrans) { }
+    //设置装备
+    public  virtual  void SetEquipDatas(List<int> equips) { }
+
+
+
+}
+
+public class roleModelSprite : BaseModelSprite
+{
+
+
+    public override void CreateModel(string path)
+    {
+        if (null != animator)
+        {
+            model.SetActive(true);
+            return;
+        }
+
+        AssetManager.LoadAsset(path, (Object target, string comePath) => {
+            GameObject prefab = target as GameObject;
+            if (null != prefab)
+            {
+                GameObject go = GameObject.Instantiate(prefab, animator.transform);
+                model = go;
+                animator = go.GetComponent<Animator>();
+            }
+        });
+    }
+
+    public override void RemoveAnimatorView()
+    {
+        if (null != animator)
+        {
+            model.SetActive(false);
+            GameObject.Destroy(model);
+            model = null;
+            animator = null;
+            //暂时销毁处理
+            // this.StartCoroutine(timeOut());
+        }
+    }
+
+    IEnumerator timeOut()
+    {
+        yield return new WaitForSeconds(60);
+        //非活动状态 清理东西对象
+        if (!model.activeSelf)
+        {
+            if (null != model)
+            {
+                GameObject.Destroy(model);
+            }
+            model = null;
+            animator = null;
+        }
+    }
+
+    public override void Play(string animationName)
+    {
+        if (null == animator) return;
+        animator.Play(animationName);
+    }
+
+    public override void UpdatePos(Vector3 pos)
+    {
+        model.transform.localPosition = pos;
+    }
+
+    public override void UpdateRotete(Vector3 dir)
+    {
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        model.transform.localRotation = Quaternion.Euler(Vector3.up * angle);
+    }
+
+    public override void ChangeTranslucence(bool isTrans)
+    {
+        if(this.isTrans != isTrans)
+        {
+            this.isTrans = isTrans;
+            UpdateTranslucence();
+        }
+    }
+
+    private void UpdateTranslucence()
+    {
+        if (null == animator) return;
+        if (isTrans)
+        {
+            animator.gameObject.SetActive(true);
+            SkinnedMeshRenderer render = animator.gameObject.transform.Find("equitPos").GetComponent<SkinnedMeshRenderer>();
+
+            if (!isTrans)
+            {
+                render.material.shader = Shader.Find("Custom/PengLuOccTransVF");
+            }
+            else
+            {
+                render.material.shader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
+                Color color = render.material.color;
+                render.material.color = new Color(color.r, color.g, color.b, 125);
+            }
+
+        }
+        else
+        {
+            animator.gameObject.SetActive(false);
+        }
+    }
+
+    public override void SetEquipDatas(List<int> equips)
+    {
+
+    }
+}
+
--- Assets/Scripts/Character/ZTAnimator.cs
+++ Assets/Scripts/Character/ZTAnimator.cs
@@ -6,128 +6,70 @@ using XLua;
 [LuaCallCSharp]
 public class ZTAnimator : MonoBehaviour {
      
-    private Animation _animator; //动画
-    private GameObject _animatorGo; //动画对象
-    private bool _isTrans;  //是否半透明
+
+    private BaseModelSprite modelSprite;
 
     public void Start()
     {
-        _animator = null;
-        _animatorGo = null;
-        _isTrans = false;
-    }
 
-   
+    }
 
     public void OnDestroy()
     {
-        //if (null != _animatorGo)
-        //{
-        //    GameObject.Destroy(_animatorGo);
-        //}
-        _animatorGo = null;
-        _animator = null;
+        if (modelSprite == null) return;
+        modelSprite.RemoveAnimatorView();
     }
 
     //创建形象
     public void CreateAnimatorView(string path)
     {
-        if (null != _animator)
-        {
-            this.gameObject.SetActive(true);
-            return;
-        }
+        if (modelSprite == null)
+            modelSprite = new roleModelSprite();
+        modelSprite.CreateModel(path);
+    }
 
-        AssetManager.LoadAsset(path, (Object target, string comePath) => {
-            GameObject prefab = target as GameObject;
-            if (null != prefab)
-            {
-                GameObject go = GameObject.Instantiate(prefab,this.gameObject.transform);
-                _animatorGo = go;
-                _animator = go.GetComponent<Animation>();
-            }
-        });
+    public void SetEquips(List<int> equips)
+    {
+        if (modelSprite == null) return;
+        modelSprite.SetEquipDatas(equips);
     }
 
     //移除形象
     public void RemoveAnimatorView()
     {
-        if (null != _animator)
-        {
-            this.gameObject.SetActive(false);
-            this.StartCoroutine(timeOut());
-        }
-    }
-
-    IEnumerator timeOut()
-    {
-        yield return new WaitForSeconds(60);
-        //非活动状态 清理东西对象
-        if (!this.gameObject.activeSelf)
-        {
-            if (null != _animatorGo)
-            {
-                GameObject.Destroy(_animatorGo);
-            }
-            _animatorGo = null;
-            _animator = null;
-        }
+        if (modelSprite == null) return;
+        modelSprite.RemoveAnimatorView();
     }
 
     //播放动作
     public void Play(string actoinName)
     {
-        if (null == _animator) return;
-        _animator.Play(actoinName);
+        if (modelSprite == null) return;
+        modelSprite.Play(actoinName);
     }
 
     //刷新位置
     public void UpdatePos(Vector3 pos)
     {
-        this.gameObject.transform.localPosition = pos;
+        if (modelSprite == null) return;
+        modelSprite.UpdatePos(pos);
     }
 
     //更新角度
     public void UpdateRotate(Vector3 dir)
     {
-        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
-        this.gameObject.transform.localRotation = Quaternion.Euler(Vector3.up * angle);
+        if (modelSprite == null) return;
+        modelSprite.UpdateRotete(dir);
     }
 
     //改变透明度
     public void ChangeTranslucence(bool isTrans)
     {
-        if(_isTrans != isTrans)
-        {
-            _isTrans = isTrans;
-            UpdateTranslucence();
-        }
-       
-    }
-    //刷新透明度
-    private void UpdateTranslucence()
-    {
-        if (null == _animator) return;
-        if (_isTrans)
-        {
-            _animator.gameObject.SetActive(true);
-            SkinnedMeshRenderer render = _animator.gameObject.transform.Find("equitPos").GetComponent<SkinnedMeshRenderer>();
+        if (modelSprite == null) return;
+        modelSprite.ChangeTranslucence(isTrans);
 
-            if (!_isTrans)
-            {
-                render.material.shader = Shader.Find("Custom/PengLuOccTransVF");
-            }
-            else
-            {
-                render.material.shader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
-                Color color = render.material.color;
-                render.material.color = new Color(color.r, color.g, color.b, 125);
-            }
-
-        }
-        else
-        {
-            _animator.gameObject.SetActive(false);
-        }
     }
+
+    
+    
 }
