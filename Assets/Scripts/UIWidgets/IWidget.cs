aa3246336e58275e7c50f999b3454e0fe0fc41df
ClaineLe <7941324@qq.com>
2018-03-16 11:01:06 +0800

    增加新的UI控件

---
+++ Assets/Scripts/UIWidgets/IWidget.cs
@@ -0,0 +1,14 @@
+﻿
+public interface IWidget
+{
+	#if UNITY_EDITOR 
+	void InitEditor(string paramStr);
+	#endif
+	
+	//初始化控件
+	void Init (string paramStr);
+
+	WidgetAnimation WidgetAnim{ get;}
+
+
+}
---
+++ Assets/Scripts/UIWidgets/WidgetAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WidgetAnimation
+{
+	private WidgetAnimation()
+	{
+	}
+
+	public static WidgetAnimation Create(GameObject widget)
+	{
+		return new WidgetAnimation ();
+	}
+}
