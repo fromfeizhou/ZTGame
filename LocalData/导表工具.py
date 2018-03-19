437b8afb032beb1d025570796880b3be663c0199
fromfeizhou <763457385@qq.com>
2018-03-19 18:45:30 +0800

    【工具】 python 客户端导表工具

--- LocalData/导表工具.py
+++ LocalData/导表工具.py
@@ -4,6 +4,18 @@ import os
 import xlrd
 g_root_path = os.getcwd()
 
+def ToArrTab(value,isStr):
+	arr = value.split('^')
+	res = '{'
+	for i in range(0, len(arr)):
+		if isStr:
+			res = res +('\'%s\',' % arr[i])
+		else:
+			res = res + '%d,' % int(arr[i])
+	res = res +'}'
+	print res
+	return res
+
 def writeLua(fname):
 	print fname
  	try:
@@ -16,18 +28,18 @@ def writeLua(fname):
  		# ctype: 0 empty, 1 string, 2 number, 3 date, 4 boolean, 5 error
  		workbook = xlrd.open_workbook(fname)
  		booksheet = workbook.sheets()[0]
- 		# 遍历第一行的所有列 保存数据类型
- 		for col in range(0, booksheet.ncols):
- 			cell = booksheet.cell(0, col)
- 			col_val_type_list.append(str(cell.value))
- 			assert cell.ctype == 1, "found a invalid col val in col [%d] !~" % (col)
- 		# 遍历第三行的所有列 保存字段名
+ 		# 遍历第3行的所有列 保存数据类型
  		for col in range(0, booksheet.ncols):
  			cell = booksheet.cell(2, col)
+ 			col_val_type_list.append(str(cell.value))
+ 			assert cell.ctype == 1, "found a invalid col val in col [%d] !~" % (col)
+ 		# 遍历第4行的所有列 保存字段名
+ 		for col in range(0, booksheet.ncols):
+ 			cell = booksheet.cell(3, col)
  			col_name_list.append(str(cell.value))
  			assert cell.ctype == 1, "found a invalid col name in col [%d] !~" % (col)
  		# 剔除表头、字段名和字段类型所在行 从第四行开始遍历 构造行数据
- 		for row in range(3, booksheet.nrows):
+ 		for row in range(4, booksheet.nrows):
  			# 保存数据索引 默认第一列为id
  			cell_id = booksheet.cell(row, 0)
  			assert cell_id.ctype == 2, "found a invalid id in row [%d] !~" % (row)
@@ -40,13 +52,13 @@ def writeLua(fname):
 
 			# 保存每一行的所有数据
 			for col in range(0, booksheet.ncols):
+				
 				cell = booksheet.cell(row, col)
 				k = col_name_list[col]
 				cell_val_type = col_val_type_list[col]
 				# ignored the string that start with '_'
 				if str(k).startswith('_'):
 					continue
-
 				# 根据字段类型去调整数值 如果为空值 依据字段类型 填上默认值
 				if cell_val_type == 'string':
 					if cell.ctype == 0:
@@ -63,13 +75,20 @@ def writeLua(fname):
 						v = -1
 					else:
 						v = float(cell.value)
-				elif cell_val_type == 'table':
+				elif cell_val_type == '[int]':
 					if cell.ctype == 0:
 						v = '{}'
 					else:
-						v = cell.value
+						print cell.value
+						v = ToArrTab(cell.value,False)
+				elif cell_val_type == '[string]':
+					if cell.ctype == 0:
+						v = '{}'
+					else:
+						print cell.value
+						v = ToArrTab(cell.value,True)
 				else:
-					v = cell.value
+					v = int(cell.value)
 				# 加入列表
 				row_data_list.append([k, v])
 			# 保存id 和 row data
