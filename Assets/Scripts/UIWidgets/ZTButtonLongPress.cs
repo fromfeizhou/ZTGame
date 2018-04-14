b8584a9d15bd62ba04729c3314852402fb4cfc6b
ClaineLe <7941324@qq.com>
2018-04-14 10:51:45 +0800

    增加长按按钮

---
+++ Assets/Scripts/UIWidgets/ZTButtonLongPress.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ZTButtonLongPress : Button,IWidget
+{
+	public WidgetAnimation WidgetAnim {
+		get {
+			throw new System.NotImplementedException ();
+		}
+	}
+
+	#if UNITY_EDITOR
+	public void InitEditor (string paramStr)
+	{
+	}
+	#endif
+
+	public void Init (string paramStr)
+	{
+		
+	}
+
+	public UnityAction onUp;
+	public UnityAction onDown;
+
+	// 当按钮失去焦点
+	public override void OnPointerClick (PointerEventData eventData)
+	{
+	}
+
+	public override void OnPointerUp (PointerEventData eventData)
+	{
+		base.OnPointerUp (eventData);
+		if (onUp != null)
+			onUp.Invoke();
+	}
+
+	public override void OnPointerDown(PointerEventData eventData)
+	{
+		base.OnPointerDown (eventData);
+		if (onDown != null)
+			onDown.Invoke ();
+	}
+
+	[System.Serializable]
+	public class ButtonOnUp : UnityEvent
+	{
+	}
+
+	[System.Serializable]
+	public class ButtonOnDown : UnityEvent
+	{
+	}
+}
