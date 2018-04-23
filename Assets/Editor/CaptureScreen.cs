1e128de49e67434f87964c7b9332c0791c22ffff
ClaineLe <7941324@qq.com>
2018-04-23 20:20:20 +0800

    1.增加，地图编辑器截屏功能 2.修改，替换当前平面地图 3.修改，地图卷轴缩放条比率

--- Assets/Editor/CaptureScreen.cs
+++ Assets/Editor/CaptureScreen.cs
@@ -5,16 +5,16 @@ using UnityEditor;
 using System.IO;
 
 public class CaptureScreenHelper{
-	[MenuItem ("Tools/CaptureScreen")]
+	[MenuItem ("ZTTool/CaptureScreen")]
 	public static void CaptureScreen(){
-		if (Selection.objects [0].GetType () == typeof(GameObject)) {
-			GameObject cameraGo = Selection.objects [0] as GameObject;
-			if (cameraGo != null && cameraGo.tag.Equals("CaptureScreen")) {
-				Camera camera = cameraGo.GetComponent<Camera> ();
-				if (camera != null) {
-					Texture2D tex2D = GameTool.CaptureScreen (camera);
-					File.WriteAllBytes ("CaptureScreen.png", tex2D.EncodeToPNG ());
-				}
+		GameObject cameraGo = GameObject.FindGameObjectWithTag("CaptureScreen");
+		if (cameraGo != null && cameraGo.tag.Equals("CaptureScreen")) {
+			Camera camera = cameraGo.GetComponent<Camera> ();
+			if (camera != null) {
+				Texture2D tex2D = GameTool.CaptureScreen (camera);
+				string path = Application.dataPath + "/ResourcesLib/Images/PanelPng/Map/map.png";
+				File.WriteAllBytes (path, tex2D.EncodeToPNG ());
+				AssetDatabase.Refresh ();
 			}
 		}
 	}
