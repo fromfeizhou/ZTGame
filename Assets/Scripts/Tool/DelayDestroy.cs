7c77531a32dc09afca1fb92b2e2aec6fa17700c5
ClaineLe <7941324@qq.com>
2018-04-25 19:26:03 +0800

    1.增加，延迟销毁对象脚本 2.增加，技能按钮特效

---
+++ Assets/Scripts/Tool/DelayDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayDestroy : MonoBehaviour {
+
+	public float DelayDestroyTime = 1.0f;
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (DelayDestroyTime);
+		Destroy(gameObject);
+	}
+}
