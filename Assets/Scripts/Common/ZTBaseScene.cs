4245d3a0f9153fb57047ced8cceff91993e24b75
fromfeizhou <763457385@qq.com>
2018-04-25 21:37:56 +0800

    【场景】 添加场景管理

---
+++ Assets/Scripts/Common/ZTBaseScene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[LuaCallCSharp]
+public class ZTBaseScene : MonoBehaviour
+{
+    private Action<float> _update;
+    private Action _destroy;
+    public void SetUpdate(Action<float> update)
+    {
+        _update = update;
+    }
+    public void SetDestroy(Action destroy)
+    {
+        _destroy = destroy;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != _update)
+        {
+            _update(Time.deltaTime);
+        }
+    }
+
+
+    void OnDestroy()
+    {
+        if (null != _destroy)
+        {
+            _destroy();
+        }
+    }
+}
