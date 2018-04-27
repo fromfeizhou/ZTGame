5ac3cf4c665a7301ac4339635c456511f3306d2a
ClaineLe <7941324@qq.com>
2018-04-27 18:14:17 +0800

    1.增加，编辑器反射类

---
+++ Assets/Editor/Reflection/RefHelper/RefUnityEditr.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace ReflectionUnityEditr
+{
+	public class RefUnityEditr
+	{
+		private RefHelper _ref;
+		public object refObject{
+			get{ 
+				return _ref.refObject;
+			}
+		}
+
+		public const BindingFlags DefBindingFlags = BindingFlags.Public | BindingFlags.Instance;
+		public RefUnityEditr (string className, object[] constructorParams = null)
+		{
+			_ref = new ReflectionUnityEditr.RefHelper (typeof(UnityEditor.AssetImporter), className, constructorParams);
+		}
+
+		public bool SetFieldValue (string fieldName, object fieldValue, bool igoneType = false, BindingFlags binding = DefBindingFlags)
+		{
+			return _ref.SetFieldValue (fieldName, fieldValue, binding, igoneType);
+		}
+
+		public bool GetFieldValue<T> (string fieldName, ref T value, bool igoneType = false, BindingFlags binding = DefBindingFlags)
+		{
+			return _ref.GetFieldValue<T> (fieldName, ref value, binding, igoneType);
+		}
+
+		public bool SetPropertyValue (string propertyName, object propertyValue, bool igoneType = false, object[] index = null, BindingFlags binding = DefBindingFlags)
+		{
+			return _ref.SetPropertyValue (propertyName, propertyValue, binding, igoneType, index);
+		}
+
+		public bool GetPropertyValue<T> (string propertyName, ref T value, bool igoneType = false, object[] index = null, BindingFlags binding = DefBindingFlags)
+		{
+			return _ref.GetPropertyValue<T> (propertyName, ref value, binding, igoneType, index);
+		}
+
+		public bool GetMethod(string methodName, ref MethodInfo methodInfo, Type[] types = null)
+		{
+			return _ref.GetMethod (methodName, ref methodInfo, types);
+		}
+
+		public bool GetMethods(string methodName, ref MethodInfo methodInfo, BindingFlags binding = DefBindingFlags)
+		{
+			return _ref.GetMethod (methodName, ref methodInfo, binding | BindingFlags.Instance );
+		}
+	}   
+}
+	
---
+++ Assets/Editor/Reflection/RefUnityEditor/ZoomableArea.cs
@@ -0,0 +1,107 @@
+﻿using System.Reflection;
+using ReflectionUnityEditr;
+using UnityEngine;
+
+namespace ReflectionUnityEditr{
+	public class ZoomableArea : RefUnityEditr{
+		public static ZoomableArea Create(bool minimalGUI, bool enableSliderZoom){
+			object[] constructorParams = new []{
+				(object)minimalGUI,
+				(object)enableSliderZoom
+			};
+			return new ZoomableArea (constructorParams);
+		}
+		private ZoomableArea(object[] constructorParams):base("ZoomableArea",constructorParams){
+		}
+
+		public enum YDirection
+		{
+			Positive,
+			Negative
+		}
+
+		public bool uniformScale{
+			get{
+				bool tmpValue = false;
+				if (GetPropertyValue ("uniformScale", ref tmpValue)) {
+					return tmpValue;
+				}
+				return tmpValue;
+			}
+			set{ 
+				SetPropertyValue ("uniformScale", value);
+			}
+		}
+
+
+
+		private YDirection m_UpDirection = YDirection.Positive;
+		public YDirection upDirection
+		{
+			get
+			{
+				int tmpValue = 0;
+				if (GetPropertyValue<int> ("upDirection", ref tmpValue, true)) {
+					m_UpDirection = (YDirection)tmpValue;
+				}
+				return m_UpDirection;
+			}
+			set
+			{
+				SetPropertyValue ("upDirection", (int)value, true);
+			}
+		}
+	
+		public Vector2 scale{
+			get{ 
+				Vector2 tmpValue = default(Vector2);
+				if (GetPropertyValue ("scale", ref tmpValue)) {
+					return tmpValue;
+				}
+				return tmpValue;
+			}
+		}
+
+		public Rect shownAreaInsideMargins{
+			get{ 
+				Rect tmpValue = default(Rect);
+				if (GetPropertyValue ("shownAreaInsideMargins", ref tmpValue)) {
+					return tmpValue;
+				}
+				return tmpValue;
+			}set{ 
+				SetPropertyValue ("shownAreaInsideMargins", value);
+			}
+		}
+	
+		public void Test(){
+			MethodInfo methodInfo = null;
+			if (GetMethod ("DrawingToViewTransformPoint",ref methodInfo,new []{typeof(Vector2)})) {
+				Debug.Log (methodInfo.ReturnType);
+				foreach (ParameterInfo pi in methodInfo.GetParameters()) {
+					Debug.Log ("[DrawingToViewTransformPoint]" + pi.ParameterType);
+				}
+			}
+		}
+
+		public void Test1(){
+			MethodInfo methodInfo = null;
+			if (GetMethods ("IsPanEvent",ref methodInfo,BindingFlags.NonPublic)) {
+				Debug.Log (methodInfo.ReturnType);
+				foreach (ParameterInfo pi in methodInfo.GetParameters()) {
+					Debug.Log ("[DrawingToViewTransformPoint]" + pi.ParameterType);
+				}
+			}
+		}
+
+		public void Test2()
+		{
+			BindingFlags binding = BindingFlags.Static | BindingFlags.NonPublic;
+
+			Debug.Log (binding == BindingFlags.Static);
+			Debug.Log (binding == BindingFlags.Public);
+			Debug.Log (binding == BindingFlags.NonPublic);
+		}
+
+	}
+}
